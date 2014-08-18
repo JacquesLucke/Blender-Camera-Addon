45163e051de70e1d1c04ff2e8a9fa74b1597901a
Jacques Lucke <mail@jlucke.com>
2014-08-18 21:22:24 +0200

    parent camera to circle

--- rotating camera rig creation.py
+++ rotating camera rig creation.py
@@ -9,17 +9,22 @@ def insertRotatingCamera():
     targetControler = bpy.context.object
     targetControler.empty_draw_size = 0.2
     
-    bpy.ops.object.empty_add(location = [0, 0, 1.5], type = "CIRCLE")
+    bpy.ops.mesh.primitive_circle_add(location = [0, 0, 1.5])
     positionControler = bpy.context.object
     positionControler.scale = [5, 5, 5]
-    positionControler.rotation_euler.x = 90 / 180 * math.pi
     
     bpy.ops.object.camera_add(location = [0, -5, 1.5])
     camera = bpy.context.object
     
+    bpy.ops.object.select_all(action = "DESELECT")
     camera.select = True
     bpy.context.scene.objects.active = targetControler
     bpy.ops.object.track_set(type = "TRACKTO")
+    
+    bpy.ops.object.select_all(action = "DESELECT")
+    camera.select = True
+    bpy.context.scene.objects.active = positionControler
+    bpy.ops.object.parent_set(type = "OBJECT", keep_transform = True)
     
     
 insertRotatingCamera()
