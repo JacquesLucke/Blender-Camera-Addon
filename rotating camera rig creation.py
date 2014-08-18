14dd35ed74cee02de8efe1b1f6bf29a3bb1361d5
Jacques Lucke <mail@jlucke.com>
2014-08-18 19:50:14 +0200

    create camera + big circle

--- rotating camera rig creation.py
+++ rotating camera rig creation.py
@@ -1,6 +1,7 @@
-import bpy
+import bpy, math
 
 def insertRotatingCamera():
+    
     bpy.ops.mesh.primitive_circle_add(location = [0, 0, -2])
     control = bpy.ops.object
     
@@ -8,6 +9,13 @@ def insertRotatingCamera():
     positionControler = bpy.context.object
     positionControler.empty_draw_size = 0.2
     
+    bpy.ops.object.empty_add(location = [0, 0, 1.5], type = "CIRCLE")
+    targetControler = bpy.context.object
+    targetControler.scale = [5, 5, 5]
+    targetControler.rotation_euler.x = 90 / 180 * math.pi
+    
+    bpy.ops.object.camera_add(location = [0, -5, 1.5])
+    camera = bpy.context.object
     
     
 insertRotatingCamera()
