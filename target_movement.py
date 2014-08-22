a76e6c015230b591c0a84cd2abad71682e72a109
Jacques Lucke <mail@jlucke.com>
2014-08-22 13:01:19 +0200

    get Target Camera if one exists

--- target_movement.py
+++ target_movement.py
@@ -4,14 +4,17 @@ from utils import *
 cameraRigPropertyName = "Camera Rig Type"
 targetCameraType = "TARGET" 
 
+targetCameraSaver = None
+
 def insertTargetMovementCamera():
-	camera = newCamera()
-	movement = newEmpty(name = "Movement Empty", location = [0, 0, 0])
-	
-	setParentWithoutInverse(camera, movement)
-	camera.location.z = 4
-	
-	markAllAsPartOfTargetCamera(camera, movement)
+	if not getTargetCamera():
+		camera = newCamera()
+		movement = newEmpty(name = "Movement Empty", location = [0, 0, 0])
+		
+		setParentWithoutInverse(camera, movement)
+		camera.location.z = 4
+		
+		setCustomProperty(camera, cameraRigPropertyName, targetCameraType)
 
 def newCamera():
 	bpy.ops.object.camera_add(location = [0, 0, 0])
@@ -19,12 +22,41 @@ def newCamera():
 	camera.name = "Target Camera"
 	camera.rotation_euler = [0, 0, 0]
 	return camera
+
+def getTargetCamera():
+	global targetCameraSaver
+	if targetCameraSaver is not None:
+		if not targetCameraSaver.get(cameraRigPropertyName) == targetCameraType:
+			targetCameraSaver = None
+	if targetCameraSaver is None:
+		for object in bpy.data.objects:
+			if isTargetCamera(object):
+				targetCameraSaver = object
+	return targetCameraSaver
+				
+			
+def isTargetCamera(camera):
+	if camera:
+		if camera.get(cameraRigPropertyName) == targetCameraType:
+			return True
+	return False
+
+# interface
+
+class TargetCameraPanel(bpy.types.Panel):
+	bl_space_type = "VIEW_3D"
+	bl_region_type = "TOOLS"
+	bl_category = "Animation"
+	bl_label = "Target Camera"
+	bl_context = "objectmode"
 	
-def markAllAsPartOfTargetCamera(camera, movement):
-	setCustomProperty(camera, cameraRigPropertyName, targetCameraType)
-	setCustomProperty(movement, cameraRigPropertyName, targetCameraType)
-
-
+	@classmethod
+	def poll(self, context):
+		return getTargetCamera()
+	
+	def draw(self, context):
+		layout = self.layout
+	
 # operators
 		
 class AddTargetMovementCamera(bpy.types.Operator):
